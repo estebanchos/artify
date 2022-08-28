7f3764f6c8824bd3715d134c409f2ddce61255f9
Carlos Ocampo <charlie@carlosocampo.ca>
2022-08-28 12:21:34 -0500

    feat: authentication and jwt cookie implemented

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,28 +1,48 @@
-import { sign } from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 import { serialize } from "cookie"
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+const bcrypt = require('bcryptjs')
 
 const secret = process.env.SECRET
 
 export default async function (req, res) {
     const { email, password } = req.body
     console.log(email, password)
-    // check and validate user
-    //  need to return 401 if user is not validated
 
+    // validate inputs
+    if (!email || !password) return res.status(400).json({
+        message: 'Please enter the required fields',
+        success: false
+    });
+    // validate user exists
+    const userFound = await prisma.user.findUnique({
+        where: {
+            email: email
+        },
+    })
+    if (!userFound) return res.status(400).json({
+        message: 'Invalid user',
+        success: false
+    });
+    // validate password is correct
+    const isPasswordCorrect = bcrypt.compareSync(password, userFound.password)
+    if (!isPasswordCorrect) return res.status(400).json({
+        message: 'Invalid password',
+        success: false
+    });
 
-    // token should return role??
-    let id = '123'
-    let role = 'test'
-    const token = sign(
+    // create token and serialize it
+    const token = jwt.sign(
         {
             exp: Math.floor(Date.now() / 1000) + (60 * 60),
-            id: id,
-            role: role
+            email: userFound.email,
+            role: userFound.role
         },
         secret
     )
 
-    const serialised = serialize('artify', token, {
+    const serialized = serialize('artify', token, {
         httpOnly: true,
         secure: 'development',
         sameSite: 'strict',
@@ -30,7 +50,7 @@ export default async function (req, res) {
         path: '/',
     })
 
-    res.setHeader('Set-Cookie', serialised)
+    res.setHeader('Set-Cookie', serialized)
     res.status(200).send('Successful login')
-    
+
 }
