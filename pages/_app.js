36c055189115be151d401f1c8866cc775734fe0f
Carlos Ocampo <charlie@carlosocampo.ca>
2022-08-28 17:38:27 -0500

    added useContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import Layout from '../components/Layout'
+import AppContext from '../context/AppContext'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContext.Provider>
   )
 }
 
