d1f4c7cf1acdd520314c23b618afb29db4d06d7e
Carlos Ocampo <charlie@carlosocampo.ca>
2022-08-31 14:42:36 -0500

    minor styling changes

--- components/ArtUpload.js
+++ components/ArtUpload.js
@@ -4,6 +4,7 @@ import { DatePicker, notification } from 'antd';
 import { UploadOutlined, PictureOutlined, CheckCircleOutlined } from '@ant-design/icons'
 import styles from '../styles/ArtUpload.module.css'
 import AppContext from "../context/AppContext";
+import Head from "next/head";
 
 const uploadNotification = (type) => {
     if (type === 'success') {
@@ -86,74 +87,81 @@ function ArtUpload() {
     }
 
     return (
-        <section className={styles.formContainer}>
-            <h2 className={styles.title}>Add a piece of art to Artify</h2>
-            <form className={styles.form} onSubmit={handleSubmit}>
-                {role === 'ARTIST' ?
-                    ''
-                    :
+        <>
+            <section className={styles.formContainer}>
+                <h2 className={styles.title}>Upload a piece of art</h2>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    {role === 'ARTIST' ?
+                        ''
+                        :
+                        <div className={styles.inputContainer}>
+                            <select
+                                className={styles.dropdown}
+                                name='artist'
+                                id='artist'
+                                value={artistId}
+                                onChange={(e) => setArtistId(e.target.value)}
+                            >
+                                <option value='' disabled hidden>Select Artist</option>
+                                {artists?.map((artist) => {
+                                    return (
+                                        <option
+                                            className={styles.dropdownOptions}
+                                            key={artist.id}
+                                            value={artist.id}>
+                                            {artist.name}
+                                        </option>
+                                    )
+                                })}
+                            </select>
+                        </div>
+                    }
                     <div className={styles.inputContainer}>
-                        <select
-                            className={styles.dropdown}
-                            name='artist'
-                            id='artist'
-                            value={artistId}
-                            onChange={(e) => setArtistId(e.target.value)}
-                        >
-                            <option value='' disabled hidden>Select Artist</option>
-                            {artists?.map((artist) => {
-                                return (
-                                    <option className={styles.dropdownOptions} key={artist.id} value={artist.id}>{artist.name}</option>
-                                )
-                            })}
-                        </select>
-                    </div>
-                }
-                <div className={styles.inputContainer}>
-                    <label className={styles.label} htmlFor='title'>Art Title</label>
-                    <input
-                        id='title'
-                        name='title'
-                        className={styles.titleInput}
-                        placeholder='Mona Lisa'
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        autoCapitalize='none'
-                    />
-                </div>
-                <div className={styles.inputContainer}>
-                    <label className={styles.label} htmlFor='creationDate'>Creation Date</label>
-                    <div className={styles.dateContainer}>
-                        <DatePicker
-                            onChange={(_date, dateString) => {
-                                setCreationDate(dateString);
-                            }}
-                            size='large'
+                        <label className={styles.label} htmlFor='title'>Art Title</label>
+                        <input
+                            id='title'
+                            name='title'
+                            className={styles.titleInput}
+                            placeholder='Title'
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            autoCapitalize='none'
                         />
                     </div>
-                </div>
-                <div className={styles.inputContainer}>
-                    <span className={styles.label}>Image</span>
-                    <input
-                        id='image'
-                        name='image'
-                        accept="image/*"
-                        type='file'
-                        className={styles.imageInput}
-                        onChange={onChangeImage}
-                    />
-                    <label className={styles.labelImage} htmlFor='image'><PictureOutlined /> Choose Image</label>
-                    {imageName ?
-                        <span className={styles.imageName}><span className={styles.successIcon}><CheckCircleOutlined /></span>{imageName}</span>
-                        : ''}
-                </div>
-                <button className={styles.buttonContainer}>
-                    <UploadOutlined /> Upload
-                </button>
-                {/* <div className={styles.buttonContainer}>
+                    <div className={styles.inputContainer}>
+                        <label className={styles.label} htmlFor='creationDate'>Creation Date</label>
+                        <div className={styles.dateContainer}>
+                            <DatePicker
+                                onChange={(_date, dateString) => {
+                                    setCreationDate(dateString);
+                                }}
+                                size='large'
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <span className={styles.label}>Image</span>
+                        <input
+                            id='image'
+                            name='image'
+                            accept="image/*"
+                            type='file'
+                            className={styles.imageInput}
+                            onChange={onChangeImage}
+                        />
+                        <label className={styles.labelImage} htmlFor='image'><PictureOutlined /> Choose Image</label>
+                        {imageName ?
+                            <span className={styles.imageName}><span className={styles.successIcon}><CheckCircleOutlined /></span>{imageName}</span>
+                            : ''}
+                    </div>
+                    <button className={styles.buttonContainer}>
+                        <UploadOutlined /> Upload
+                    </button>
+                    {/* <div className={styles.buttonContainer}>
                 </div> */}
-            </form>
-        </section>
+                </form>
+            </section>
+        </>
     );
 }
 
