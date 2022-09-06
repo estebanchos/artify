3aaf215129c4eb6c7efaab01a3bf20a3a4fa4cb5
Carlos Ocampo <charlie@carlosocampo.ca>
2022-09-05 22:19:21 -0500

    minor style changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen space-y-8 lg-flex-row'>
       <Head>
-        <title>Artify</title>
+        <title>Artify - Sell and Manage your art</title>
         <meta name="Art Management Tool" content="Artify helps artists manage their art and collectors find art" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
